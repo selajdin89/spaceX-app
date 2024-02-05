b37ffe23edd10cb093cf13673a652f68fd0c23ce
selajdin89 <selajdinmemeti89@gmail.com>
2024-02-05 14:46:17 +0100

    Route config and Navbar created

---
+++ src/pages/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='nav-bar'>
+      <nav>
+        <ul>
+          <NavLink>Rockets</NavLink>
+          <NavLink>Missions</NavLink>
+          <NavLink>My Profile</NavLink>
+          <NavLink>Dragons</NavLink>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
