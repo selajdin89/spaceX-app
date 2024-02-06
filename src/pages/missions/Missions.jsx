c53e1a31efc2d9ab06547b05b62f167f2e7f9742
Fjolla2002 <fjolla.selimi17@gmail.com>
2024-02-06 11:10:13 +0100

    Finalized task of joining and leaving missions

--- src/pages/missions/Missions.jsx
+++ src/pages/missions/Missions.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setMissions } from '../../redux/missions/missionSlice';
-import axios from 'axios'
+import { setMissions, joinMission, leaveMission } from '../../redux/missions/missionSlice';
+import axios from 'axios';
+import './missions.css';
 ;
 const Missions = () => {
   const dispatch = useDispatch();
@@ -19,15 +20,23 @@ const Missions = () => {
     }
   }, [dispatch]);
 
+  const handleJoinMission = (mission_id) => {
+    dispatch(joinMission({mission_id}))
+  };
+
+  const handleLeaveMission = (mission_id) => {
+    dispatch(leaveMission({mission_id}))
+  };
+
   return (
-    <div>
-      <h2>Missions</h2>
-      <table>
+    <div className="missions">
+      <table className="missions-content">
         <thead>
           <tr>
-            <th>Mission ID</th>
-            <th>Mission Name</th>
+            <th>ID</th>
+            <th>Name</th>
             <th>Description</th>
+            <th>Join/Leave</th>
           </tr>
         </thead>
         <tbody>
@@ -35,7 +44,26 @@ const Missions = () => {
             <tr key={mission.mission_id}>
               <td>{mission.mission_id}</td>
               <td>{mission.mission_name}</td>
-              <td>{mission.description}</td>
+              <td className="mission-desc">{mission.description}</td>
+              <td>
+                {
+                  mission.reserved ? (
+                    <>
+                      <span>Active Member</span>
+                      <button type="button" className="btn" onClick={() => handleLeaveMission(mission.mission_id)}>
+                        Leave
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <span>Not a Member</span>
+                      <button type="button" className="btn" onClick={() => handleJoinMission(mission.mission_id)}>
+                        Join
+                      </button>
+                    </>
+                  )
+                }
+              </td>
             </tr>
           ))}
         </tbody>
--- src/redux/missions/missionSlice.jsx
+++ src/redux/missions/missionSlice.jsx
@@ -4,9 +4,27 @@ const missionsSlice = createSlice({
   name: 'missions',
   initialState: [],
   reducers: {
-    setMissions: (state, action) => action.payload.map(({ mission_id, mission_name, description }) => ({ mission_id, mission_name, description })),
+    setMissions: (state, action) =>
+      action.payload.map(({ mission_id, mission_name, description }) => ({
+        mission_id,
+        mission_name,
+        description,
+        reserved: false,
+      })),
+    joinMission: (state, action) => {
+      const { mission_id } = action.payload;
+      return state.map((mission) =>
+        mission.mission_id === mission_id ? { ...mission, reserved: true } : mission
+      );
+    },
+    leaveMission: (state, action) => {
+      const { mission_id } = action.payload;
+      return state.map((mission) =>
+        mission.mission_id === mission_id ? { ...mission, reserved: false } : mission
+      );
+    },
   },
 });
 
-export const { setMissions } = missionsSlice.actions;
+export const { setMissions, joinMission, leaveMission } = missionsSlice.actions;
 export default missionsSlice.reducer;
